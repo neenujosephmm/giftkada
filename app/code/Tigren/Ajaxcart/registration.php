72cdf662dfd1985f573c03a2da0c208c7280ac20
midhung123 <midhun@ceymox.com>
2020-04-22 12:18:37 +0530

    509685,cms

--- app/code/Tigren/Ajaxcart/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Tigren_Ajaxcart', __DIR__);
--- app/code/Tigren/Ajaxsuite/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @copyright Copyright (c) 2017 www.tigren.com
- */
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Tigren_Ajaxsuite', __DIR__);
